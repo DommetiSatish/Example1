8d39422147567918faed803d4e2ff8c7b66a39d2
Satish <satishdommeti22@gmail.com>
2019-02-10 22:20:13 +0530

    first

---
+++ src/main/java/com/Candidate/Dao/CandidateDao.java
@@ -0,0 +1,8 @@
+package com.Candidate.Dao;
+
+import com.Candidate.Model.Candidate;
+
+public interface CandidateDao {
+boolean createCandidateDao(Candidate candidate);
+
+}
---
+++ src/main/java/com/Candidate/Dao/CandidateDaoImpl.java
@@ -0,0 +1,12 @@
+package com.Candidate.Dao;
+
+import com.Candidate.Model.Candidate;
+
+
+public class CandidateDaoImpl implements CandidateDao {
+
+	public boolean createCandidateDao(Candidate candidate) {
+		return ;
+	}
+
+}
